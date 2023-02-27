7eb024520fce8f3f785ed1e36009b2d11ccc4de9
Mark de Jong <Fristi@users.noreply.github.com>
2023-02-27 16:07:54 +0100

    Working linked WASM host <-> guest!

--- app/src/lib.rs
+++ app/src/lib.rs
@@ -1,13 +1,28 @@
 wit_bindgen::generate!("git" in "../wit/git.wit");
 
+use serde::Deserialize;
+
 struct MyGitlog;
 
+#[derive(Deserialize)]
+struct IpData {
+    ip: String
+}
+
 impl exports::Exports for MyGitlog {
-    fn enrich(commit: exports::Commit) -> Result<exports::Enrichment,String> {
+    fn enrich(commit: exports::Commit) -> anyhow::Result<exports::Enrichment,String> {
         if commit.message.contains("TECH") {
             return Ok(exports::Enrichment::Link("http://vectos.net".to_string()))
         } else if commit.message.contains("IP") {
-            todo!()
+            let headers: [(&str, &str); 0] = [];
+            let resp = http::send( http::HttpRequest { url: "https://api.ipify.org?format=json", headers: &headers });
+
+            if resp.status == 200 {
+                let ip: IpData = serde_json::from_slice(resp.body.as_slice()).unwrap();
+                return Ok(exports::Enrichment::Link(ip.ip));
+            } else {
+                return Ok(exports::Enrichment::None)
+            }
         } else {
             return Ok(exports::Enrichment::None)
         }
--- runner/src/main.rs
+++ runner/src/main.rs
@@ -1,13 +1,25 @@
 // generate bindings.
 wasmtime::component::bindgen!({ world: "git", path: "../wit/git.wit" });
 use std::fmt::{Display, Formatter};
+use std::io::Read;
 
 use anyhow::*;
 use wasmtime::{component::{Component, Linker}, Engine, Store, Config};
-// Acutally, when you run `cargo b` in the last step, you will get a wasm module,
-// not a wasm component, so we need to use this ComponentEncoder to transform the
-// wasm module to component.
 use wit_component::ComponentEncoder;
+
+pub struct Context {
+    http: ReqwestHttp
+}
+
+impl Context {
+    pub fn new() -> Self {
+        Context { http: ReqwestHttp { } }
+    }
+
+    pub fn http(&mut self) -> &mut ReqwestHttp {
+        &mut self.http
+    }
+}
 
 fn main() -> Result<()> {
     let mut config = Config::new();
@@ -15,8 +27,11 @@ fn main() -> Result<()> {
     config.wasm_component_model(true);
 
     let engine = Engine::new(&config)?;
-    let mut store = Store::new(&engine, 0);
-    let linker = Linker::new(&engine);
+    let mut store: Store<Context> = Store::new(&engine, Context::new());
+    let mut linker: Linker<Context> = Linker::new(&engine);
+
+    http::add_to_linker(&mut linker, Context::http)?;
+
     let wasi_adapter = include_bytes!("../../wit/wasi_snapshot_preview1.wasm");
 
     // we first read the bytes of the wasm module.
@@ -28,14 +43,37 @@ fn main() -> Result<()> {
         .validate(true)
         .adapter("wasi_snapshot_preview1", wasi_adapter)?
         .encode()?;
-    std::fs::write("./target/component.wasm", &component)?;
+
     let component = Component::from_binary(&engine, &component)?;
 
     // after getting the component, we can instantiate a markdown instance.
     let (gitlog, _) = Gitlog::instantiate(&mut store, &component, &linker)?;
-    let res = gitlog.exports.call_enrich(&mut store, exports::Commit{ message: "TECH: added stuff", timestamp: 0})?.unwrap();
+    let res = gitlog.exports.call_enrich(&mut store, exports::Commit{ message: "IP: test", timestamp: 0})?.unwrap();
     println!("Got enrichment: {}", res);
     Ok(())
+}
+
+pub struct ReqwestHttp;
+
+impl http::Http for ReqwestHttp {
+
+
+    fn send(&mut self, req:http::HttpRequest) -> anyhow::Result<http::HttpResponse> {
+
+        let client = reqwest::blocking::Client::new();
+
+        let mut builder = client.get(req.url);
+
+        for (k, v) in req.headers {
+            builder = builder.header(k, v);
+        }
+
+        let mut resp = builder.send()?;
+        let mut buf: Vec<u8> = vec![];
+        resp.copy_to(&mut buf)?;
+
+        Ok(http::HttpResponse{ status: resp.status().as_u16(), body: buf })
+    }
 }
 
 impl Display for exports::Enrichment {
